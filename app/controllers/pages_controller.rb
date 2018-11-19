dd66a3bc0f89e0256689cb1c27d2d723e92f39ec
Steve Knight <stevejknight@hotmail.co.uk>
2018-11-19 13:48:08 +0000

    spree installed at /shop, links working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def shop
+  end
+
+  def club
+  end
+
+  def member
+  end
 end
---
+++ config/initializers/devise.rb
@@ -0,0 +1,1 @@
+Devise.secret_key = "256efa56f3ca2a0c642bed0f74a1e77f008ee33e1a2a6e08a23bc2aec487c01cb322c82d6af87f04a4ff52fc600dad317b78"
